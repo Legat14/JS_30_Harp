1a7a99dc81f7c6d9815d95d2d09d954a35165292
Legat14 <zia-d@mail,ru>
2021-09-25 17:24:25 +0400

    refactor: change princip of add and remove audio

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-const harpNotes = ['C2', 'Db2', 'D2']; //Назначаем все ноты для конкретной гармошки
+const harpNotes = ['C2', 'Db2', 'D2']; // Назначаем все ноты для конкретной гармошки
 
 function buttonClick() {
-  if (!event.repeat) { //Если событие уже запущено, то оно не повторяется.
-    //Это нужно для того, чтобы избежать многократного проигрывания звука при зажатой клавише.
-    if ((event.code == 'KeyQ' && event.type == 'keydown') || event.type == 'mousedown') { //Звук должен заиграть только при нажатии определенной клавиши или
-      //щелчка левой кнопкой мыши по кнопке
+  if (!event.repeat) { // Если событие уже запущено, то оно не повторяется
+                       // Это нужно для того, чтобы избежать многократного проигрывания звука при зажатой клавише
+    if ((event.code == 'KeyQ' && event.type == 'keydown') || event.type == 'mousedown') { // Звук должен заиграть только при нажатии
+                                                                                         // определенной клавиши или щелчка левой кнопкой
+                                                                                         // мыши по кнопке ноты
 
-      const playNote = new Audio();
-
-      playNote.src = '../sounds/C2.mp3';
-      playNote.preload = 'auto';
-      //playNote.loop = 'auto'; Отключаю повтор, пока не удастся найти способ сделать зацикленный звук более естественным.
-      playNote.volume = 0.2; //Громкость звука. В последствии нужно сделать общий параметр, управляемый ползунком.
-
-      playNote.play();
-}
-    else if (event.code == 'KeyQ' && event.type == 'keyup') {
-      console.log(event.type);
+      const playNote = document.createElement('audio'); // Создаем автоматически проигрываемый звук в html
+      playNote.setAttribute('src', 'sounds/C2.mp3');
+      playNote.setAttribute('preload', '');
+      playNote.setAttribute('autoplay', '');
+      playNote.volume = 0.05; // Громкость звука. В последствии нужно сделать общий параметр, управляемый ползунком
+      document.body.append(playNote);
     }
-}
+  }
 }
 
-const buttonSelection = document.querySelector('button');
-buttonSelection.addEventListener('mousedown', buttonClick);
+function buttonUnclick() { // Удаляем все звуки из html при отпускании клавиши клавиатуры
+                           // Это нужно чтобы прервать проигрывание звука и не засорять html
+  if (event.code == 'KeyQ' && event.type == 'keyup') {
+    const audioSelection = document.querySelectorAll('audio');
+    audioSelection.forEach((element, i) => {
+      audioSelection[i].remove();
+    })
+  }
+}
+
+const buttonSelection = document.querySelector('button'); // Выбираем пока что единственную кнопку
+buttonSelection.addEventListener('mousedown', buttonClick); // Слушаем клики мыши, нажатие или отпускание клавиш
 document.addEventListener('keydown', buttonClick);
-document.addEventListener('keyup', buttonClick);
+document.addEventListener('keyup', buttonUnclick);
