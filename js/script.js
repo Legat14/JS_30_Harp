f74d3615b48bd291f87e652b97edc8cd40b45c6d
Legat14 <zia-d@mail,ru>
2021-09-30 18:08:05 +0400

    feat: add all harps and notes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,65 @@
-// Назначаем все ноты для конкретной гармошки
-const harpNotes = ['C2', 'Db2', 'D2', 'E2', 'F2', 'Gb2', 'G2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'E3', 'F3', 'G3',
-  'Ab3', 'A3', 'B3', 'C4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'A4', 'Bb4', 'B4', 'C5'
-];
+'use strict';
+
+// Выбираем строй гармошки из C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B
+const harpType = 'C';
+
+// Назначаем все ноты, в зависимости от выбранной гармошки harpType
+let harpNotes;
+switch(harpType) {
+  case 'C':
+    harpNotes = ['C2', 'Db2', 'D2', 'E2', 'F2', 'Gb2', 'G2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'E3', 'F3', 'G3',
+    'Ab3', 'A3', 'B3', 'C4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'A4', 'Bb4', 'B4', 'C5'];
+    break;
+  case 'Db':
+    harpNotes = ['Db2', 'D2', 'Eb2', 'F2', 'Gb2', 'G2', 'Ab2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'F3', 'Gb3', 'Ab3',
+    'A3', 'Bb3', 'C4', 'Db4', 'Eb4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'Bb4', 'B4', 'C5', 'Db5'];
+    break;
+  case 'D':
+    harpNotes = ['D2', 'Eb2', 'E2', 'Gb2', 'G2', 'Ab2', 'A2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'Gb3', 'G3', 'A3',
+    'Bb3', 'B3', 'Db4', 'D4', 'E4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'B4', 'C5', 'Db5', 'D5'];
+    break;
+  case 'Eb':
+    harpNotes = ['Eb2', 'E2', 'F2', 'G2', 'Ab2', 'A2', 'Bb2', 'Bb2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'G3', 'Ab3', 'Bb3',
+    'B3', 'C4', 'D4', 'Eb4', 'F4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'C5', 'Db5', 'D5', 'Eb5'];
+    break;
+  case 'E':
+    harpNotes = ['E2', 'F2', 'Gb2', 'Ab2', 'A2', 'Bb2', 'B2', 'B2', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'Ab3', 'A3', 'B3',
+    'C4', 'Db4', 'Eb4', 'E4', 'Gb4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'Db5', 'D5', 'Eb5', 'E5'];
+    break;
+  case 'F':
+    harpNotes = ['F2', 'Gb2', 'G2', 'A2', 'Bb2', 'B2', 'C3', 'C3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'A3', 'Bb3', 'C4',
+    'Db4', 'D4', 'E4', 'F4', 'G4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'D5', 'Eb5', 'E5', 'F5'];
+    break;
+  case 'Gb':
+    harpNotes = ['Gb2', 'G2', 'Ab2', 'Bb2', 'B2', 'C3', 'Db3', 'Db3', 'D3', 'Eb3', 'E3', 'F3', 'Gb3', 'G3', 'Ab3', 'Bb3', 'B3', 'Db4',
+    'D4', 'Eb4', 'F4', 'Gb4', 'Ab4', 'A4', 'Bb4', 'B4', 'C5', 'Db5', 'Eb5', 'E5', 'F5', 'Gb5'];
+    break;
+  case 'G':
+    harpNotes = ['G1', 'Ab1', 'A1', 'B1', 'C2', 'Db2', 'D2', 'D2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'B2', 'C3', 'D3',
+    'Eb3', 'E3', 'Gb3', 'G3', 'A3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'E4', 'F4', 'Gb4', 'G4'];
+    break;
+  case 'Ab':
+    harpNotes = ['Ab1', 'A1', 'Bb1', 'C2', 'Db2', 'D2', 'Eb2', 'Eb2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'C3', 'Db3', 'Eb3',
+    'E3', 'F3', 'G3', 'Ab3', 'Bb3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'F4', 'Gb4', 'G4', 'Ab4'];
+    break;
+  case 'A':
+    harpNotes = ['A1', 'Bb1', 'B1', 'Db2', 'D2', 'Eb2', 'E2', 'E2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'Db3', 'D3', 'E3',
+    'F3', 'Gb3', 'Ab3', 'A3', 'B3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'Gb4', 'G4', 'Ab4', 'A4'];
+    break;
+  case 'Bb':
+    harpNotes = ['Bb1', 'B1', 'C2', 'D2', 'Eb2', 'E2', 'F2', 'F2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'D3', 'Eb3', 'F3',
+    'Gb3', 'G3', 'A3', 'Bb3', 'C4', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'G4', 'Ab4', 'A4', 'Bb4'];
+  case 'B':
+    harpNotes = ['B1', 'C2', 'Db2', 'Eb2', 'E2', 'F2', 'Gb2', 'Gb2', 'G2', 'Ab2', 'A2', 'Bb2', 'B2', 'C3', 'Db3', 'Eb3', 'E3', 'Gb3',
+    'G3', 'Ab3', 'Bb3', 'B3', 'Db4', 'D4', 'Eb4', 'E4', 'F4', 'Gb4', 'Ab4', 'A4', 'Bb4', 'B4'];
+    break;
+}
 
 // Назначаем горячие клавиши. Каждая горячая клавиша в массиве проигрывает ноту, соответствующую ей
 // по индексу в массиве harpKeys. То есть hotKey[0] проигрывает ноту harpNotes
 const hotKeys = ['KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'KeyA', 'KeyS', 'KeyD',
   'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma',
-  'Period', 'Slash'
-];
+  'Period', 'Slash'];
 
 // Вводим значения ширины и высоты поля, на котором будут располагаться кнопки, размеры кнопок и отступы между ними
 // Для быстрого изменения размещения кнопок
@@ -61,6 +112,7 @@ function buttonClick() {
       // Звук должен заиграть только при нажатии определенной клавиши или щелчка левой кнопкой
       // мыши по кнопке ноты
 
+      let i = 0;
       if (event.type == 'mousedown' && event.which == 1) {
         // Находим индекс кнопки, по которой кликнули
         const targetClick = event.target;
@@ -105,11 +157,12 @@ function buttonUnclick() { // Удаляем все звуки из html при 
     // При таком удалении аудио удаляется только последнее аудио. Это позволяет играть быстро, но
     // есть баг - если нажать две или более кнопок и одну отпустить, то удалится первый нажаты звук
     // не зависимо от того, какая клавиша была отпущена
-    audioSelection = document.querySelector('audio');
+    let audioSelection = document.querySelector('audio');
     if (audioSelection != null) {
       audioSelection.remove();
     }
 
+    let i = 0;
     if (event.type == 'mouseout') {
       // Находим индекс кнопки, по которой кликнули
       const targetClick = event.target;
@@ -139,7 +192,7 @@ function radioButtonSelected() {
           document.body.children[0].children[1].children[j].children[1].style.backgroundColor = "";
           document.body.children[0].children[1].children[j].children[1].style.boxShadow = "";
         }
-        for (k = i - 1; k >= 0; k--) {
+        for (let k = i - 1; k >= 0; k--) {
           if (k <= 3) { // Проверка на зеленый цвет
             document.body.children[0].children[1].children[k].children[1].style.backgroundColor = "#20ff02";
             document.body.children[0].children[1].children[k].children[1].style.boxShadow = "0px 0px 5px 2px #4eff48";
@@ -164,7 +217,7 @@ function radioButtonSelected() {
 
         // Удаляем звук щелчка после проигрывания, чтобы не мешал потом удалять звуки нот
         setTimeout(() => {
-            audioSelection = document.querySelector('audio');
+            let audioSelection = document.querySelector('audio');
             if (audioSelection != null) {
               audioSelection.remove();
             }
